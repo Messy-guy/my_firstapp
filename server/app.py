3b168b51209f9838a0e586e4265a1c9f8e80ae86
ChiefBibek <114321426+ChiefBibek@users.noreply.github.com>
2024-08-27 23:58:55 +0545

    database of mongo db set up

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , request , jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 import config
@@ -6,19 +6,21 @@ import config
 app = Flask(__name__)
 CORS(app)
 
+# Initialize MongoDB client and database
 client = MongoClient(config.MONGO_URL)
 db = client['userInput']
 users_collection = db['users']
 
 @app.route('/addUser', methods=['POST'])
 def add_user():
+    print('hello')
     data = request.json
     name = data.get('name')
     job = data.get('job')
     specialities = data.get('specialities')
 
     if not data:
-        return jsonify({'error': 'datas are required'}), 400
+        return jsonify({'error': 'Data is required'}), 400
 
     user = {'name': name, 'job': job, 'specialities': specialities}
     users_collection.insert_one(user)
@@ -29,3 +31,6 @@ def add_user():
 def get_users():
     users = list(users_collection.find({}, {'_id': 0}))  # Exclude MongoDB ID
     return jsonify(users), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=10000)
---
+++ server/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+def get_users():
+#     users = list(users_collection.find({}, {'_id': 0}))  # Exclude MongoDB ID
+#     return jsonify(users), 200
